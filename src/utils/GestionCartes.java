778fddbdad5c5ff5a21262974b9bcd80f621b362
h1tlex <89601876+h1tlex@users.noreply.github.com>
2025-03-06 15:12:28 +0100

    improved GestionCartes class

--- src/utils/GestionCartes.java
+++ src/utils/GestionCartes.java
@@ -1,59 +1,55 @@
 package utils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
 public class GestionCartes {
 
-	public static <T> T extraire(T[] array){
-		if (array.length == 0) {
+	public static <T> T extraire(List<T> array){
+		if (array.isEmpty()) {
             throw new IllegalArgumentException("Array is empty");
         }
-		
 		Random rand = new Random();
-		int i = rand.nextInt(array.length);
-        T removedElement = array[i]; // Store element to return
-        
-        // Create a new array excluding the removed element
-        T[] newArray = Arrays.copyOf(array, array.length - 1);
-
-        for (int j = i; j < newArray.length; j++) {
-            newArray[j] = array[j + 1]; // Shift elements left
-        }
-        
-		return removedElement;
+		int i = rand.nextInt(array.size());
+		return array.remove(i);
 	}
 	
-	public static <T> T[] melanger(T[] array) {
+	public static <T> List<T> melanger(List<T> array) {
 		Random rand = new Random();
 
-        for (int i = 0; i < array.length; i++) {
+        for (int i = array.size(); i > 0; i--) {
         	int j = rand.nextInt(i+1);
-			T temp = array[j];
-			array[j] = array[i];
-			array[i] = temp; // swap
+			T temp = array.get(i);
+			array.set(i, array.get(j));
+			array.set(j, temp); // swap
 		}
         
 		return array;
 	}
 	
-	//TODO FIX COLLECTIONS IMPORT
-	public static <T> boolean verifierMelanger(T[] array1, T[] array2) {
-		if (array1.length != array2.length) {
+	public static <T> boolean verifierMelanger(List<T> array1, List<T> array2) {
+		if (array1.size() != array2.size()) {
 			return false;
 		}
 		
-		List<T> list1 = Arrays.asList(array1);
-        List<T> list2 = Arrays.asList(array2);
-		
-		for(T element : list1) {
-			if (Collections.frequency(list1,element) != Collections.frequency(list2, element)) {
+		for(T element : array1) {
+			if (Collections.frequency(array1,element) != Collections.frequency(array2, element)) {
 				return false;
 			}
 		}
 		return true;
 	}
-
+	
+	//TODO FIGURE THIS SHIT OUT
+	public static <T> List<T> rassembler(List<T> array) {
+		List<T> newArray = new ArrayList<>();
+		for (int i = 0; i < array.size(); i++) {
+			array.addAll(i,newArray);
+		}
+		return newArray;
+	}
 }
